da15918b474b69d41912f05f7d0ac677db1db729
eka-martin <eka.martin.kupris@gmail.com>
2023-08-23 14:22:46 +0200

    index/server creation

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="pt-5 my-5 text-body-secondary border-top">
+    Created by the Bootstrap team &middot; &copy; 2023
+  </footer>
+</div>
+<script src="/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+    </body>
+</html>
